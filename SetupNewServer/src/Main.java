8a2105bafa446258f82445a8eb09db6a546558dc
Alien <neptunesblade@gmail.com>
2017-02-17 20:43:03 -0800

    Newest main file.

--- SetupNewServer/src/Main.java
+++ SetupNewServer/src/Main.java
@@ -1,36 +1,85 @@
+import java.io.*;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main
 {
-    public static final String mainFolderPath = "C:\\Users\\calin\\Desktop\\Servers";
-    public static Scanner input;
+    private static final String mainFolderPath = "C:\\Users\\calin\\Desktop\\Servers\\";
+    private static final String presetsFolderPath = mainFolderPath + "Presets\\";
+    private static final String profilesFolderPath = mainFolderPath + "Profiles\\";
+    private static String[] vendors = {"Mojang", "Spigot", "Mod Packs"};
+    private static String[] vendorNotes = {" - In Testing", " - Not Yet Implemented", " - Not Yet Implemented"};
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        //Get profile name
         System.out.println("Enter profile name: ");
         String profileName = input.nextLine().replace("\n", "").replace("\r", "").trim();
-
+        //Get vendor
+        System.out.println("Enter Vendor: ");
+        for (int i = 0; i < vendors.length; i++)
+        {
+            System.out.println(i + 1 + ") " + vendors[i] + vendorNotes[i]);
+        }
+        int vendorIndex = Integer.parseInt(input.nextLine()) - 1;
+        String vendor = vendors[vendorIndex];
+        //Get server version
+        System.out.println("Enter the server version: ");
+        String version = input.nextLine().replace("\n", "").replace("\r", "").trim();
+        verifyPresetDownload(vendor, version);
+        File outputPath = new File(profilesFolderPath + "\\" + profileName + "\\" + version);
+        outputPath.mkdirs();
+        File outputFilePath = new File(profilesFolderPath + "\\" + profileName + "\\" + version + "\\" + version + ".jar");
+        Files.copy(new File(presetsFolderPath + vendor + "\\" + version + ".jar").toPath(), outputFilePath.toPath()); //TODO: Catch already created server
     }
 
-
-    //Both smaller -- current
-    //Adjacent is larger, return that index
-    //Choose right side if both are greater
-    //Avoid Bounds Errors
-
-    public static boolean CreateProfile(String profileName)
+    //Verify that the given server version from given vendor is already downloaded; if not, do so
+    public static void verifyPresetDownload(String vendor, String version) throws IOException //TODO: Get a better name
     {
-        profileName = profileName.toLowerCase().trim().replace("\n", "").replace("\r", "");
-        return false;
+        File versionList = new File(presetsFolderPath + vendor + "\\" + "versionList.txt");
+        List<String> versions = Files.readAllLines(Paths.get(versionList.toString()), StandardCharsets.UTF_8);
+        //Check if jar is already downloaded
+        for(String str : versions)
+        {
+            if (str.equals(version))
+            {
+                //If jar is already downloaded, say so, and return
+                System.out.println("[#][Already Found MineCraft Server Version " + version + " From " + vendor + "]");
+                return;
+            }
+        }
+        //If jar is not already downloaded, download it
+        downloadNewPreset(vendor, version);
+        //Add new jar version to versionList.txt
+        PrintWriter versionListWriter = new PrintWriter(versionList);
+        versions.add(version);
+        for(String str : versions)
+        {
+            versionListWriter.println(str);
+        }
+        versionListWriter.close();
     }
 
-    public static boolean CreateNewServer(String profileName, String type, String version)
+    public static void downloadNewPreset(String vendor, String version) throws IOException //TODO: Add downloads for non-mojang
     {
-        //Profile names are guaranteed to only contain letters, numbers and underscores.
-        profileName = profileName.toLowerCase().trim().replace("\n", "").replace("\r", "");
-        String profilePath = mainFolderPath + "\\" + profileName;
-        //Server newServer = new Server(type, version);
-        return false;
+        System.out.println("[+][Downloading MineCraft server version " + version + " from " + vendor + "]");
+        if (vendor.equals("Mojang"))
+        {
+            URL website = new URL("https://s3.amazonaws.com/Minecraft.Download/versions/" + version + "/minecraft_server." + version + ".jar");
+            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+            FileOutputStream fos = new FileOutputStream(presetsFolderPath + vendor + "\\" + version + ".jar");
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        }
+        else
+        {
+            System.out.println("[!][" + vendor + " is not yet implemented]");
+        }
     }
 }
